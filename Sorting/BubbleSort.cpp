5c56238b2137ba7880b53ea737a19c5ed5695759
Nusrat Nory <95858180+n-o-r-y@users.noreply.github.com>
2022-02-19 00:13:40 +0600

    Create BubbleSort.cpp

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,17 @@
+void bubble_sort(int list[], int size)
+{
+
+    int temp, i, j;
+    for (i = 0; i < size; i++)
+    {
+        for (j = i; j < size; j++)
+        {
+            if (list[i] > list[j])
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+    }
+}
