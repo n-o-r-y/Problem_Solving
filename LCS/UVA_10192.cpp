b1bf589a52362487f0216ff4a06cddce0625834a
Nusrat Jahan Nory <95858180+n-o-r-y@users.noreply.github.com>
2022-03-31 23:26:50 +0600

    Create UVA_10192.cpp

---
+++ LCS/UVA_10192.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s1[1001];
+    char s2[1001];
+    int c=1;
+    while (gets(s1) != NULL) {
+        //scanf("%s %s", s1, s2);
+        if(s1[0]=='#')
+        {
+            break;
+        }
+        else
+        {
+         gets(s2);
+        int l1 = strlen(s1);
+        int l2 = strlen(s2);
+        int lcs[l1 + 1][l2 + 1];
+        for (int i = 0; i <= l1; i++)
+        {
+            for (int j = 0; j <= l2; j++)
+            {
+                lcs[i][j] = 0;//sob gula index er value 0
+            }
+        }
+        for (int i = 1; i <= l1; i++)
+        {
+            for (int j = 1; j <= l2; j++)
+            {
+                if (s1[i - 1] == s2[j - 1]) //jodi value same hoy
+                {
+
+                    lcs[i][j] = lcs[i - 1][j - 1] + 1; //diagonal er sathe +1
+
+                }
+                else
+                {
+                    lcs[i][j] = max(lcs[i][j - 1], lcs[i - 1][j]); //same na hoile upor er ta r paser ta check boro konta
+                }
+            }
+        }
+        printf("Case #%d: you can visit at most %d cities.\n", c,lcs[l1][l2]);
+        c++;
+    }
+}
+    return 0;
+}
