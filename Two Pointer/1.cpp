457377ee105ca93eb05f3eb86319c320668d6e2b
Nusrat Jahan Nory <95858180+n-o-r-y@users.noreply.github.com>
2022-12-29 21:18:05 +0600

    Create 1.cpp
    1st I have to sort the arr
    i=0,j=n-1
    2nd check arr[I]+arr[j]=x
    if noo then check is it smaller than x or larger than x
    
    if(sum>x) j--
    else i++

---
+++ Two Pointer/1.cpp
@@ -0,0 +1,49 @@
+//@using pointer find the pair int the array whose sum in equal to x
+#include<bits/stdc++.h>
+using namespace std;
+int v1,v2;
+bool solved(int n,int arr[],int x)
+{
+    int i=0;
+    int j=n-1;
+    while(i<j)
+    {
+        int sum=arr[i]+arr[j];
+        if(sum==x)
+        {
+            v1=arr[i];
+            v2=arr[j];
+            return true;
+        }
+        if(sum>x)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n+10];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int x;
+    scanf("%d",&x);
+    int ans=solved(n,arr,x);
+    if(ans==true)
+    {
+        printf("%d %d\n",v1,v2);
+    }
+    else
+    {
+        printf("Naiiii");
+    }
+}
